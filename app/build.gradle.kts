9a9af22b76f1776af2097475bd742f8f53750095
MirTheDragon <plasmabee11@gmail.com>
2025-01-26 18:18:33 +0100

    New morse code representation in log and metrics view, implemented farnsworth method to trainer and multi character checkboxes to the layout for future use.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 19
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
